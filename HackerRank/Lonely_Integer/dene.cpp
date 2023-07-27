74f4ae1b3c9c350f95908ff45e0b035dafbeaea4
aysuoner <aysuoner5@gmail.com>
2023-07-27 23:39:09 +0300

    XOR-gate

---
+++ HackerRank/Lonely_Integer/dene.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int lonelyinteger(std::vector<int>& arr) {
+    int uniqueElement = 0;
+    for (int num : arr) {
+        uniqueElement ^= num;
+    }
+    return uniqueElement;
+}
+
+int main()
+{
+	vector<int> vector1 = {1, 2, 3, 4, 3, 2, 1};
+
+	int res = lonelyinteger(vector1);
+	std::cout << res << std::endl;
+    return 0;
+}

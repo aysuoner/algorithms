d24197220568c8c258f5f09d85912e3cb3589f43
aysuoner <aysuoner5@gmail.com>
2023-08-14 14:40:36 +0300

    valid_palindrome

---
+++ LeetCode/Valid_palindrome/dene.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+bool isPalindrome(string s)
+{
+	int i = 0;
+	int j = s.size() - 1;
+	while(i < j)
+	{
+	    while(i < s.size() && !isalnum(s[i]))
+			i++;
+	    while(j >= 0 && !isalnum(s[j]))
+			j--;
+	    if(i >= j)
+			break;
+	    if(tolower(s[i]) != tolower(s[j]))
+			return false;
+	    i++;
+		j--;
+	}
+	return true;
+}
+
+int main()
+{
+	string s = "^^+^''^p9^+^";
+
+	bool res = isPalindrome(s);
+
+	cout << "res: " << res << endl;
+}

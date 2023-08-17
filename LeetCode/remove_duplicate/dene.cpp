9eda19bdbe9edcbd2d2ddf43625d930a4fe69400
aysuoner <aysuoner5@gmail.com>
2023-08-17 13:52:02 +0300

    remove_duplicate_v2

--- LeetCode/remove_duplicate/dene.cpp
+++ LeetCode/remove_duplicate/dene.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-int removeDuplicates(vector<int>& nums)
+/* int removeDuplicates(vector<int>& nums)
     {
         for (int i = 0; i < nums.size() - 1; i++)
         {
@@ -25,10 +25,42 @@ int removeDuplicates(vector<int>& nums)
 		std::cout << std::endl;
 		std::cout << nums.size();
         return (nums.size());
-    }
+    } */
+
+int removeDuplicates(vector<int>& nums)
+{
+	int i = 0;
+	int j = 0;
+	int len = nums.size();
+	if (len >= 1)
+	{
+		nums[i] = nums[j];
+		j++;
+	}
+	else
+		return 0;
+	while (j < len)
+	{
+		if (nums[i] != nums[j])
+		{
+			i++;
+			nums[i] = nums[j];
+		}
+		j++;
+	}
+	while(len > i + 1)
+	{
+		nums.pop_back();
+		len--;
+	}
+	return i + 1;
+}
 
 int main()
 {
-		vector<int> vector1 = {1, 1, 1, 1, 1, 1, 2, 3, 3, 3, 4, 5, 6, 6};
-		removeDuplicates(vector1);
+		vector<int> vector1 = {0,0,0,1,1,1,1,2,3,3,4,4,5,5,6,7,8,9,9,9};
+		int res = removeDuplicates(vector1);
+		for (int m = 0; m < vector1.size(); m++)
+			cout <<  vector1[m] << " ";
+		cout << '\n' << "res: " << res ;
 }

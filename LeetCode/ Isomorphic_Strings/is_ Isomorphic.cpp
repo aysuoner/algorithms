b08405f531a5ed9590645625af21afe19317f6db
aysuoner <aysuoner5@gmail.com>
2023-08-18 18:31:58 +0300

    is_ Isomorphic

--- LeetCode/ Isomorphic_Strings/is_ Isomorphic.cpp
+++ LeetCode/ Isomorphic_Strings/is_ Isomorphic.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
-    bool isIsomorphic(string s, string t)
-    {
-        
-    }
+bool	isIsomorphic(string s, string t)
+{
+	unordered_map<char, char> ump_s;
+	unordered_map<char, char> ump_t;
+	for (int i = 0; i < s.length(); i++)
+	{
+		if ((ump_s[s[i]] && ump_s[s[i]] != t[i]) || \
+			(ump_t[t[i]] && ump_t[t[i]] != s[i]) )
+			return false;
+		ump_s[s[i]] = t[i];
+		ump_t[t[i]] = s[i];
+	}
+	return true;
+}
 };
 

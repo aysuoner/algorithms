fe766834cd5c20cf003fea56d0df14719ed74ba7
aysuoner <aysuoner5@gmail.com>
2023-08-21 17:26:43 +0300

    binary_search_tree

--- LeetCode/Search_insert_pos/binary_search.cpp
+++ LeetCode/Search_insert_pos/binary_search.cpp
@@ -1,8 +1,22 @@
 class Solution
 {
 public:
-    int searchInsert(vector<int>& nums, int target)
+int	searchInsert(vector<int>& nums, int target)
+{
+	int	mid;
+	int	left = 0;
+	int right = nums.size() - 1;
+	mid = (right + left) / 2;
+	while (left <= right)
 	{
-        
-    }
+		mid = (right + left) / 2;
+		if (nums[mid] == target)
+			return mid;
+		else if (target > nums[mid])
+			left = mid + 1;
+		else if (target < nums[mid])
+			right = mid - 1;
+	}
+	return left;
+}
 };
